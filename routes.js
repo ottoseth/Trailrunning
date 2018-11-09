69da5a4e7ea9a1ae3c7c58794a1138e2bc4eb0cb
Seth Otto <rideopbum@netscape.net>
2018-11-09 10:56:56 -0800

    style fixes
    google maps button
    map spacing
    added descriptions

--- routes.js
+++ routes.js
@@ -4,82 +4,90 @@ var routes = {
     terrain: ["Difficult", "text-danger"],
     vertical: ["Difficult", "text-danger"],
     location: ["Cowboy Trails", "text-danger"],
-    start: "<a href='https://goo.gl/maps/XNFCGV6uiGx'>Cowboy Trails parking lot</a>",
+    start: "<a class='btn btn-outline-google' href='https://goo.gl/maps/XNFCGV6uiGx'>Cowboy Trails parking lot</a>",
     distance: 6.3,
     elev: 1104,
+    description: "Fun, sharp, 90 minute run with great views overlooking Vegas and fast crazy downhill."
   },
   cowboyhalf: {
     name: "Cowboy Half Marathon",
     terrain: ["Difficult", "text-danger"],
     vertical: ["Difficult", "text-danger"],
     location: ["Cowboy Trails", "text-danger"],
-    start: "<a href='https://goo.gl/maps/XNFCGV6uiGx'>Cowboy Trails parking lot</a>",
+    start: "<a class='btn btn-outline-google' href='https://goo.gl/maps/XNFCGV6uiGx'>Cowboy Trails parking lot</a>",
     distance: 13,
     elev: 1700,
+    description: "Half marathon (almost) loop skirting the outside of Cowboy Trails."
   },
   totheduck: {
     name: "To The Duck",
     terrain: ["Difficult", "text-danger"],
     vertical: ["Difficult", "text-danger"],
     location: ["Red Rock Canyon", "text-redrock"],
-    start: "<a href='https://goo.gl/maps/eXuZ7WSfZP82'>Calico Basin Trail parking</a>",
+    start: "<a class='btn btn-outline-google' href='https://goo.gl/maps/eXuZ7WSfZP82'>Calico Basin Trail parking</a>",
     distance: 6.8,
     elev: 2500,
+    description: "This includes the hardest climb I know of in Las Vegas, the segment called Giant's Mile, 1500 ft of elevation gain in 1 mile."
   },
   grandcircle: {
     name: "Grand Circle From Calico Basin",
     terrain: ["Medium", "text-warning"],
     vertical: ["Medium", "text-warning"],
     location: ["Calico Basin / Red Rock", "text-info"],
-    start: "<a href='https://goo.gl/maps/eXuZ7WSfZP82'>Calico Basin Trail parking</a>",
+    start: "<a class='btn btn-outline-google' href='https://goo.gl/maps/eXuZ7WSfZP82'>Calico Basin Trail parking</a>",
     distance: 14,
     elev: 1849,
+    description: "This is the classic Red Rock route touring everything the area has to offer, with a little adjustment to the starting position."
   },
   kfourhalf: {
     name: "Kevin's Four and a Half",
     terrain: ["Medium", "text-warning"],
     vertical: ["Medium", "text-warning"],
     location: ["Moenkopi Rd", "text-moenkopi"],
-    start: "<a href='https://goo.gl/maps/CU8s5GZ8Z8z'>Moenkopi Rd parking lot</a>",
+    start: "<a class='btn btn-outline-google' href='https://goo.gl/maps/CU8s5GZ8Z8z'>Moenkopi Rd parking lot</a>",
     distance: 4.3,
     elev: 530,
+    description: "Medium difficulty run that gives you a taste of the desert, some rocks, some hills, some technical switchbacks. I got this one from Kevin at a monday evening Facebook group run."
   },
   charleston: {
     name: "Charleston Summit Loop",
     terrain: ["Difficult", "text-danger"],
     vertical: ["Difficult", "text-danger"],
     location: ["Mt Charleston", "text-warning"],
-    start: "<a href='https://goo.gl/maps/t4fj9pNHUuG2'>Trail Canyon Trailhead parking</a>",
+    start: "<a class='btn btn-outline-google' href='https://goo.gl/maps/t4fj9pNHUuG2'>Trail Canyon Trailhead parking</a>",
     distance: 18,
     elev: 6200,
-    description: "If you need some mountain training this is the place to go. Only 45 minutes outside Vegas but feels like a different world."
+    description: "If you’re in Vegas and want a reminder of what mountains feel like go to Mt Charleston. It’s only a 45 minute drive but it feels like a different world."
   },
   moenkopi: {
     name: "Moenkopi Lollipop",
     terrain: ["Easy", "text-success"],
     vertical: ["Easy", "text-success"],
     location: ["Calico Basin / Red Rock", "text-info"],
-    start: "<a href='https://goo.gl/maps/eXuZ7WSfZP82'>Calico Basin Trail parking</a>",
+    start: "<a class='btn btn-outline-google' href='https://goo.gl/maps/eXuZ7WSfZP82'>Calico Basin Trail parking</a>",
     distance: 5.7,
     elev: 690,
+    description: "Easy 5 miles with great views of everything the area has to offer. My normal Saturday morning sunrise cruise."
   },
   bearstwentytwo: {
     name: "Bear's 22",
     terrain: ["Easy", "text-success"],
     vertical: ["Easy", "text-success"],
     location: ["Bear's Best", "text-success"],
-    start: "<a href='https://goo.gl/maps/WCNyDQAUwK82'>Bear's Best Trailhead parking</a>",
+    start: "<a class='btn btn-outline-google' href='https://goo.gl/maps/WCNyDQAUwK82'>Bear's Best Trailhead parking</a>",
     distance: 22.3,
     elev: 2377,
+    description: "Nice 22 mile training run, not too technical, not too steep, easy cruise with views from the west side of Las Vegas."
   },
   grateful: {
     name: "Grateful 8",
     terrain: ["Medium", "text-warning"],
     vertical: ["Medium", "text-warning"],
     location: ["Cowboy Trails", "text-danger"],
-    start: "<a href='https://goo.gl/maps/XNFCGV6uiGx'>Cowboy Trails parking lot</a>",
+    start: "<a class='btn btn-outline-google' href='https://goo.gl/maps/XNFCGV6uiGx'>Cowboy Trails parking lot</a>",
     distance: 7.9,
     elev: 1182,
+    description: "Grateful for a ruggedly beautiful 8 mile run with good climbing up Bomb Voyage, nice views of Las Vegas from the overlook and an easy cruise down Fossil Canyon. You'll need the map for this one, there are some tricky intersections."
   }
 
 }
